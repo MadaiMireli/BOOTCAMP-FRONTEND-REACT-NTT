7f8b3b35415a2de018d144df2c4534b9239005ba
max <max.collazosm@gmail.com>
2024-12-12 11:28:29 -0500

    check pi

--- src/features/product/hooks/usePagination.tsx
+++ src/features/product/hooks/usePagination.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 
+// este hook deberia recibir la data y estructurarla para que genere el paginado, esto no es un hook de paginado
 export const usePagination = (initPage: number) => {
   const [currentPage, setCurrentPage] = useState(initPage);
   const handlePageChange = (newPage: number) => {
